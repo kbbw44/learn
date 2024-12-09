b915e0caa9c6cd2e727d4bf73962f1c39c063151
ka.babaev <ka.babaev@tensor.ru>
2024-12-09 13:47:52 +0300

    add task1

---
+++ Homework_1/task1.py
@@ -0,0 +1,10 @@
+def hello_world():
+    """Привет, мир!"""
+
+    # todo Здесь нужно написать код
+    hello = 'Hello'
+    world = 'world'
+    hello_and_world = hello + " " + world + "!"
+    return hello_and_world
+
+
